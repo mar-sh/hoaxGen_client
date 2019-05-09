884394be7600d84802a2ee5f1b2e2f6d9db63a8c
yploekito <yp.loekito@gmail.com>
2019-05-09 20:56:54 +0700

    loginsignup

---
+++ vue.js
@@ -0,0 +1,84 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        email:'',
+        password:'',
+        registerpage:false,
+        loginpage:false,
+        landingpage:true,
+        islogin:false
+
+    },
+    methods:{
+        tologinpage:function(){
+            this.registerpage = false,
+            this.loginpage=true,
+            this.landingpage=false
+        },
+        toregisterpage:function(){
+            this.registerpage=true,
+            this.loginpage=false,
+            this.landingpage=true
+        },
+        login:function(){
+            axios({
+                method:'post',
+                url:"http://localhost:3000/login",
+                data:{
+                    email:this.email,
+                    password:this.password
+                }
+            })
+            .then(({data})=>{
+                localStorage.setItem('token',data.token)
+                console.log(data)
+            })
+            .catch(({err})=>{
+                console.log(err)
+            })
+
+        },
+        register:function(){
+            axios({
+                method:'post',
+                url:"http://localhost:3000/register",
+                data:{
+                    email:this.email,
+                    password:this.password
+                }
+            })
+            .then(({data})=>{
+                localStorage.setItem('token',data.token)
+                console.log(data)
+            })
+            .catch(({err})=>{
+                console.log(err)
+            })
+
+        },
+        logout:function(){
+
+        }
+        
+    },
+    computed:{
+    },
+    watch:{
+    },
+    created(){
+        if(localStorage.getItem('token')){
+            this.registerpage = false
+            this.loginpage = false
+            this.landingpage = false
+            this.islogin = true
+        }
+        axios({
+            method:'',
+            url:''
+        })
+        .then(()=>{
+        })
+        .catch(()=>{
+        })
+    }
+})
