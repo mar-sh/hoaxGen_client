da79a6ac485fea1f8186ffa280f00dccbd2f17ce
yploekito <yp.loekito@gmail.com>
2019-05-09 21:29:09 +0700

    added get hoaxes

--- vue.js
+++ vue.js
@@ -6,7 +6,8 @@ const app = new Vue({
         registerpage:false,
         loginpage:false,
         landingpage:true,
-        islogin:false
+        islogin:false,
+        hoaxes:[]
 
     },
     methods:{
@@ -31,12 +32,32 @@ const app = new Vue({
             })
             .then(({data})=>{
                 localStorage.setItem('token',data.token)
+                this.showhide()
+                this.getHoaxes()
                 console.log(data)
             })
             .catch(({err})=>{
                 console.log(err)
             })
 
+        },
+        getHoaxes:function(){
+            axios({
+                method:'get',
+                url:"http://localhost:3000/hoaxes",
+                headers:{
+                    token:localStorage.getItem('token')
+                }
+            })
+            .then(({data})=>{
+                console.log(data)
+                for(let i = 0 ; i < data.hoaxes.length; i++){
+                    this.hoaxes.push(data.hoaxes[i])
+                }
+            })
+            .catch(({err})=>{
+                console.log(err)
+            })
         },
         register:function(){
             axios({
@@ -49,6 +70,8 @@ const app = new Vue({
             })
             .then(({data})=>{
                 localStorage.setItem('token',data.token)
+                this.showhide()
+
                 console.log(data)
             })
             .catch(({err})=>{
@@ -58,6 +81,18 @@ const app = new Vue({
         },
         logout:function(){
             localStorage.removeItem('token')
+            this.registerpage = false
+            this.loginpage = false
+            this.landingpage =true
+            this.islogin = false
+        },
+        showhide:function(){
+            if(localStorage.getItem('token')){
+                this.registerpage = false
+                this.loginpage = false
+                this.landingpage = false
+                this.islogin = true
+            }
         }
         
     },
