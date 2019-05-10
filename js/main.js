e389e6d48f9813df5194823dadfc4e793ed8c98b
sukmaranggapradeta <adit.netral@gmail.com>
2019-05-10 10:30:38 +0700

    rebase with dev

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const app = new Vue({
     el:'#app',
     data:{
+        email:'',
+        password:'',
+        registerpage:false,
+        loginpage:false,
+        landingpage:true,
+        islogin:false,
+        hoaxes:[],
+        page_input:false,
         name:'',
         user_name:'',
         tweet_content:'',
@@ -20,6 +28,90 @@ const app = new Vue({
         }
     },
     methods: {
+
+        tologinpage:function(){
+            this.registerpage = false,
+            this.loginpage=true,
+            this.landingpage=false
+        },
+        toregisterpage:function(){
+            this.registerpage=true,
+            this.loginpage=false,
+            this.landingpage=true
+        },
+        login:function(){
+            axios({
+                method:'post',
+                url:"http://localhost:3000/login",
+                data:{
+                    email:this.email,
+                    password:this.password
+                }
+            })
+            .then(({data})=>{
+                localStorage.setItem('token',data.token)
+                this.showhide()
+                this.getHoaxes()
+                console.log(data)
+            })
+            .catch(({err})=>{
+                console.log(err)
+            })
+
+        },
+        getHoaxes:function(){
+            axios({
+                method:'get',
+                url:"http://localhost:3000/hoaxes",
+                headers:{
+                    token:localStorage.getItem('token')
+                }
+            })
+            .then(({data})=>{
+                console.log(data)
+                for(let i = 0 ; i < data.hoaxes.length; i++){
+                    this.hoaxes.push(data.hoaxes[i])
+                }
+            })
+            .catch(({err})=>{
+                console.log(err)
+            })
+        },
+        register:function(){
+            axios({
+                method:'post',
+                url:"http://localhost:3000/register",
+                data:{
+                    email:this.email,
+                    password:this.password
+                }
+            })
+            .then(({data})=>{
+                localStorage.setItem('token',data.token)
+                this.showhide()
+
+                console.log(data)
+            })
+            .catch(({err})=>{
+                console.log(err)
+            })
+
+        },
+        logout:function(){
+            localStorage.removeItem('token')
+            this.registerpage = false
+            this.loginpage = false
+            this.landingpage =true
+            this.islogin = false
+        },
+        showhide:function(){
+            if(localStorage.getItem('token')){
+                this.registerpage = false
+                this.loginpage = false
+                this.landingpage = false
+                this.islogin = true
+            }
+        },
         checkLikes(value){
             if(value > 1e9){
                 
@@ -46,4 +138,20 @@ const app = new Vue({
             }
         }
     },
+    created(){
+        if(localStorage.getItem('token')){
+            this.registerpage = false
+            this.loginpage = false
+            this.landingpage = false
+            this.islogin = true
+        }
+        axios({
+            method:'',
+            url:''
+        })
+        .then(()=>{
+        })
+        .catch(()=>{
+        })
+    }
 })
