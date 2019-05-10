689aea6b3c1687b24d79c6c6648f7b4c92690916
Dion <dionagoes@gmail.com>
2019-05-10 09:11:12 +0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        name: "dion",
+        account: "dionmichaaael",
+        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem error blanditiis architecto eum optio eius id ex voluptatibus. Ipsam, labore.",
+        hour: "",
+        minute: "",
+        ampm: ""
+    }
+})
+
+console.log(app)
